46aac4cef4d30662b22542f7434a3e8b3c987b91
Sirius1616 <siriusa1.615@gmail.com>
2025-09-13 19:43:44 +0100

    Dir update

--- 02_programme_task/rtf_file_conversion.py
+++ 02_programme_task/rtf_file_conversion.py
@@ -273,7 +273,7 @@ def final_text_cleanup(text):
     return text.strip()
 
 def prepare_input_dir():
-    source_dir = CONFIG["SOURCE_DIR"]
+    source_dir = CONFIG["SOURCE_"]
     target_dir = CONFIG["INPUT_DIR"]
     os.makedirs(target_dir, exist_ok=True)
     for filepath in glob.glob(os.path.join(source_dir, "*.rtf")):

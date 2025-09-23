446b9d190ba81f0bc56082fc8dd528dc106aeac9
Sirius1616 <siriusa1.615@gmail.com>
2025-09-23 13:48:01 +0100

    file match correction

--- 05_programme_task/csv_spans_matcher.py
+++ 05_programme_task/csv_spans_matcher.py
@@ -74,7 +74,7 @@ def match_subheads_with_spans(cleaned_file, span_file, output_file):
                     "Even/Odd": ""
                 })
 
-    print(f"✅ Matching done. Output saved as: 
+    print(f"✅ Matching done. Output saved as: {output_file}")
 
 
 if __name__ == "__main__":
